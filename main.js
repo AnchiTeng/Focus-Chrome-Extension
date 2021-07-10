87902510f29ceef9b78ac076e43af383576f1949
Anchi Teng <anchi1009@gmail.com>
2021-07-11 00:01:33 +0800

    7/10

--- main.js
+++ main.js
@@ -13,7 +13,24 @@ const image = document.createElement('img');
 image.src = 'https://images.unsplash.com/photo-1533749047139-189de3cf06d3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dGltZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60';
 contentParent.appendChild(image);
 
-document.querySelector('div').onclick = function() {
+// $('div').click(function() {
+//     alert('A div was clicked!');
+//   });
+
+contentParent.onclick = function() {
     alert('A div was clicked!')
 };
+
+console.log($.ajax({
+  method : 'GET',
+  url : 'http://unsplash.it/',
+  success : function(result){
+    return result;
+  },
+  error :function(err){
+    return err;
+  }
+}));
+
+  
 
